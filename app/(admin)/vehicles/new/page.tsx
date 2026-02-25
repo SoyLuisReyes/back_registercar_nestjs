e590a5255aedb16136886e3334667f3dc01fd526
SoyLuisReyes <tecwicho@gmail.com>
2026-02-25 00:58:09 -0600

    test

--- app/(admin)/vehicles/new/page.tsx
+++ app/(admin)/vehicles/new/page.tsx
@@ -1,7 +1,13 @@
+import ProductForm from "@/components/vehicles/ProductForm";
 
 
 export default function NewVehiclePage() {
   return (
-    <div>Creando vehiculo</div>
+    <>
+        <div className="max-w-2xl mx-auto bg-white p-8 mt-10 shadow-sm border border-gray-100 rounded-lg">
+            <ProductForm/>
+
+        </div>
+    </>
   )
 }
