ef2261713011f6d37950c7100554c1f5dcef35b0
SoyLuisReyes <tecwicho@gmail.com>
2026-03-01 23:58:08 -0600

    test

---
+++ actions/updated-vehicle-actions.ts
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const updated-vehicle-actions = () => {
+  return (
+    <div>updated-vehicle-actions</div>
+  )
+}
--- components/vehicles/EditVehicleForm.tsx
+++ components/vehicles/EditVehicleForm.tsx
@@ -2,6 +2,15 @@ import React from 'react'
 
 export default function EditVehicleForm ({ children }: { children: React.ReactNode }) {
   return (
-    <div>EditVehicleForm</div>
+    <form 
+      className='space-y-5' 
+      >
+        {children}
+        <input 
+          type="submit"
+          className='rounded bg-green-400 font-bold py-2 w-full cursor-pointer'
+          value="Guardar Cambios" 
+        />
+    </form>
   )
 }
